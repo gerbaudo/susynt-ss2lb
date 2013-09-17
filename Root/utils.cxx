6befea6c91348278dba90f1a7f0933b88a903f36
Davide Gerbaudo <davide.gerbaudo@gmail.com>
2013-09-16 18:39:16 -0700

    move implementation to cxx, avoid multiple definition err

--- Root/utils.cxx
+++ Root/utils.cxx
@@ -22,3 +22,17 @@ std::string mkdirIfNeeded(const std::string &dirname)
   }
 }
 
+
+//http://stackoverflow.com/questions/874134/find-if-string-endswith-another-string-in-c
+bool endswith(const std::string &s, const std::string &end) {
+  if(s.length()<end.length()) return false;
+  else return (0==s.compare(s.length() - end.length(), end.length(), end));
+}
+
+bool fileExists(const std::string &filename)
+{
+  std::ifstream file(filename.c_str());
+  bool doesExists = file;
+  file.close();
+  return doesExists;
+}
--- SusyTest0/utils.h
+++ SusyTest0/utils.h
@@ -12,17 +12,8 @@
 // mkdir if it is not already there. Return dir path; return empty string if there was a problem
 std::string mkdirIfNeeded(const std::string &dirname);
 
-bool endswith(const std::string &s, const std::string &end) {
-  //http://stackoverflow.com/questions/874134/find-if-string-endswith-another-string-in-c
-  if(s.length()<end.length()) return false;
-  else return (0==s.compare(s.length() - end.length(), end.length(), end));
-}
+bool endswith(const std::string &s, const std::string &end);
 
-bool fileExists(const std::string &filename)
-{
-  std::ifstream file(filename.c_str());
-  bool doesExists = file;
-  file.close();
-  return doesExists;
-}
+bool fileExists(const std::string &filename);
+
 #endif
