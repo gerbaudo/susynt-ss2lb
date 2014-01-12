a8dcdcef271cfee7ba05b6f1d62398bde69c52ca
Davide Gerbaudo <davide.gerbaudo@gmail.com>
2014-01-11 22:10:31 -0800

    utils: define filter

--- SusyTest0/utils.h
+++ SusyTest0/utils.h
@@ -7,7 +7,7 @@
   Aug 2013
  */
 
-#include <algorithm>    // std::set_intersection
+#include <algorithm>    // std::set_intersection, remove_if
 #include <fstream>
 #include <string>
 #include <vector>
@@ -39,6 +39,21 @@ std::vector<T> subtract_vector(std::vector<T>& a, const std::vector<T>& b)
   std::set_difference(a.begin(), a.end(), b.begin(), b.end(), std::back_inserter( difference ));
   return difference;
 }
+
+// Filter a container with a predicate
+// Lifted from:
+// http://stackoverflow.com/questions/2797142/higher-order-function-filter-in-c
+template <
+  template <typename,typename> class Container,
+  typename Predicate,
+  typename Allocator,
+  typename A
+  > Container<A, Allocator> filter(Container<A, Allocator> const & container, Predicate const & pred) {
+  Container<A, Allocator> filtered(container);
+  filtered.erase(std::remove_if(filtered.begin(), filtered.end(), pred), filtered.end());
+  return filtered;
+}
+
 
 
 #endif
