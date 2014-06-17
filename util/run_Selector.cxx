bc66d093989de6a6c4d5ade5740c72e46d20985e
Davide Gerbaudo <davide.gerbaudo@gmail.com>
2014-06-16 22:15:55 -0700

    add run_Selector
    This is the main executable for the hlfv selector. For now it's mainly
    a skeleton.  Since I was tired of parsing the options over and over, I
    switched to 'getopt_long'. I still don't like its syntax, but it's
    good enough for now.
    
    Note to self: what you should implement is a thin wrapper around
    'getopt_long' providing a more friendly interface, similar to the one
    implemented in the fastjet parser
    http://fastjet.fr/repo/doxygen-3.0.6/CmdLine_8hh_source.html
    (see for example  'fastjet__timing_8.cc')

---
+++ util/run_Selector.cxx
@@ -0,0 +1,104 @@
+/**
+
+   Main executable for hlfv::Selector
+
+   davide.gerbaudo@gmail.com
+   June 2014
+*/
+
+#include "SusyntHlfv/Selector.h"
+#include "SusyNtuple/ChainHelper.h"
+#include "SusyNtuple/string_utils.h"
+
+#include "TChain.h"
+#include "Cintex/Cintex.h"
+
+
+#include <cstdlib>
+#include <string>
+#include <getopt.h>
+
+using namespace std;
+
+//----------------------------------------------------------
+void print_usage(const char *exeName) {
+    cout<<"Usage: "<<endl
+        <<exeName<<" options"<<endl
+        <<"\t -h [--help]                : print this message \n"
+        <<"\t -v [--verbose]             : toggle verbose \n"
+        <<"\t -d [--debug]               : toggle debug \n"
+        <<"\t -i [--input]   <file.txt>  : input root file (or filelist or dir) \n"
+        <<"\t -s [--sample]  <samplename>: sample name \n"
+        <<"Example: \n"
+        <<exeName<<"\n"
+        <<" -i filelist/Sherpa_CT10_lllnu_WZ_MassiveCB.txt \n"
+        <<" -s Sherpa_CT10_lllnu_WZ_MassiveCB \n"
+        <<endl;
+}
+//----------------------------------------------------------
+int main(int argc, char** argv) {
+    ROOT::Cintex::Cintex::Enable();
+    int nEvt = -1;
+    int nSkip = 0;
+    bool verbose=false;
+    bool debug=false;
+    string sample;
+    string input;
+    string output;
+
+    int opt=0;
+    static struct option long_options[] = {
+        {"help",    no_argument,       0, 'h'},
+        {"verbose", no_argument,       0, 'v'},
+        {"debug",   no_argument,       0, 'd'},
+        {"input",   required_argument, 0, 'i'},
+        {"sample",  required_argument, 0, 's'},
+        {0, 0, 0, 0}
+    };
+    while ((opt=getopt_long (argc, argv, "hvd:i:s:", long_options, NULL)) != -1) {
+        switch (opt) {
+        case 'h' : print_usage(argv[0]); exit(0);
+        case 'v' : verbose=true; break;
+        case 'd' : debug=true; break;
+        case 'i' : input = optarg; break;
+        case 's' : sample = optarg; break;
+        default: cout<<"unknown option "<<argv[optind]<<endl; exit(1);
+        }
+    }
+    if (optind < argc) {
+        cout<<"The following arguments are not expected; they will be discarded:"<<endl;
+        while (optind < argc) cout<<argv[optind++]<<endl;
+    }
+    if(verbose)
+        cout<<"verbose '"<<verbose<<"'"<<endl
+            <<"debug   '"<<debug  <<"'"<<endl
+            <<"input   '"<<input  <<"'"<<endl
+            <<"sample  '"<<sample <<"'"<<endl;
+
+
+    TChain* chain = new TChain("susyNt");
+    bool inputIsFile = susy::utils::endswith(input, ".root");
+    bool inputIsList = susy::utils::endswith(input, ".txt");
+    bool inputIsDir  = susy::utils::endswith(input, "/");
+    bool validInput(inputIsFile||inputIsList||inputIsDir);
+    if(!validInput) {
+        cout<<"invalid input '"<<input<<"'"<<endl;
+        print_usage(argv[0]); return 1;
+    }
+    if(!sample.size()) {
+        cout<<" sample '"<<sample<<"'"<<endl;
+        print_usage(argv[0]); return 1;
+    }
+    if(inputIsFile) ChainHelper::addFile    (chain, input);
+    if(inputIsList) ChainHelper::addFileList(chain, input);
+    if(inputIsDir ) ChainHelper::addFileDir (chain, input);
+    Long64_t nEntries = chain->GetEntries();
+    nEvt = (nEvt<0 ? nEntries : nEvt);
+    if(debug) chain->ls();
+
+    hlfv::Selector selector;
+    chain->Process(&selector, sample.c_str(), nEvt, nSkip);
+
+    delete chain;
+    return 0;
+}
