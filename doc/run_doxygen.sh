336a08ef23f9f6c5cf21b619db9e5e7699f88b20
Davide Gerbaudo <davide.gerbaudo@gmail.com>
2014-06-18 00:06:37 -0700

    test run doxygen

---
+++ doc/run_doxygen.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+if [ "$TRAVIS_REPO_SLUG" == "gerbaudo/SusyntHlfv" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_BRANCH" == "master" ]; then
+
+  echo -e "Publishing doxygen...\n"
+  doxygen  doc/doxygen.conf
+  cp -R doc/doxygen/html $HOME/doxygen-latest
+
+  cd $HOME
+  git config --global user.email "travis@travis-ci.org"
+  git config --global user.name "travis-ci"
+  git clone --quiet --branch=gh-pages https://${GH_TOKEN}@github.com/gerbaudo/SusyntHlfv gh-pages > /dev/null
+
+  cd gh-pages
+  git rm -rf ./doc/doxygen/html
+  cp -Rf $HOME/doxygen-latest ./doc/doxygen/html
+  git add -f .
+  git commit -m "Lastest doc on successful travis build $TRAVIS_BUILD_NUMBER auto-pushed to gh-pages"
+  git push -fq origin gh-pages > /dev/null
+
+  echo -e "Published doxygen doc to gh-pages.\n"
+  
+fi
