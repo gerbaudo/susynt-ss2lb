975268256b00e1a7f76af85f1fd2c6c69a0563ce
Davide Gerbaudo <davide.gerbaudo@gmail.com>
2014-09-18 15:44:12 -0700

    EventFlags: add obj-level flags

--- Root/EventFlags.cxx
+++ Root/EventFlags.cxx
@@ -8,21 +8,27 @@ using hlfv::EventFlags;
 std::string EventFlags::str() const
 {
   std::ostringstream oss;
-  oss<<" grl: "           <<grl
-     <<" larErr: "        <<larErr
-     <<" tileErr: "       <<tileErr
-     <<" ttcVeto: "       <<ttcVeto
-     <<" goodVtx: "       <<goodVtx
-     <<" tileTrip: "      <<tileTrip
-     <<" lAr: "           <<lAr
-     <<" badJet: "        <<badJet
-     <<" deadRegions: "   <<deadRegions
-     <<" badMuon: "       <<badMuon
-     <<" cosmicMuon: "    <<cosmicMuon
-     <<" hfor: "          <<hfor
-     <<" ge2blep: "       <<ge2blep
-     <<" eq2blep: "       <<eq2blep
-     <<" mllMin: "        <<mllMin;
+  oss<<" grl: "             <<grl
+     <<" larErr: "          <<larErr
+     <<" tileErr: "         <<tileErr
+     <<" ttcVeto: "         <<ttcVeto
+     <<" goodVtx: "         <<goodVtx
+     <<" tileTrip: "        <<tileTrip
+     <<" lAr: "             <<lAr
+     <<" badJet: "          <<badJet
+     <<" deadRegions: "     <<deadRegions
+     <<" badMuon: "         <<badMuon
+     <<" cosmicMuon: "      <<cosmicMuon
+     <<" hfor: "            <<hfor
+     <<" ge2blep: "         <<ge2blep
+     <<" eq2blep: "         <<eq2blep
+     <<" mllMin: "          <<mllMin
+     <<" l0eta: "           <<l0eta
+     <<" l1eta: "           <<l1eta
+     <<" tauVeto: "         <<tauVeto
+     <<" dileptonTrigger: " <<dileptonTrigger
+     <<" oppositeSign: "    <<oppositeSign
+     <<" l0pt: "            <<l0pt;
   return oss.str();
 }
 //-----------------------------------------
--- SusyntHlfv/EventFlags.h
+++ SusyntHlfv/EventFlags.h
@@ -1,4 +1,3 @@
-
 // emacs -*- C++ -*-
 #ifndef HLFV_EVENTFLAGS_H
 #define HLFV_EVENTFLAGS_H
@@ -14,23 +13,26 @@ namespace hlfv
    Feb 2014
 */
 struct EventFlags {
-  EventFlags() { reset(); }
-  void reset() {
-   grl = larErr = tileErr = ttcVeto = goodVtx = tileTrip = lAr = false;
-   badJet = deadRegions = badMuon = cosmicMuon = hfor = ge2blep = eq2blep = mllMin = false;
-   l0eta = l1eta = tauVeto = dileptonTrigger = oppositeSign = l0pt = false;
-  }
-  bool grl, larErr, tileErr, ttcVeto, goodVtx, tileTrip, lAr;
-  bool badJet, deadRegions, badMuon, cosmicMuon, hfor, ge2blep, eq2blep, mllMin;
+    EventFlags() { reset(); }
+    void reset() {
+        grl = larErr = tileErr = ttcVeto = goodVtx = tileTrip = lAr = false;
+        badJet = deadRegions = badMuon = cosmicMuon = hfor = ge2blep = eq2blep = mllMin = false;
+        l0eta = l1eta = tauVeto = dileptonTrigger = oppositeSign = l0pt = false;
+    }
+    bool grl, larErr, tileErr, ttcVeto, goodVtx, tileTrip, lAr;
+    bool badJet, deadRegions, badMuon, cosmicMuon, hfor, ge2blep, eq2blep, mllMin;
     bool l0eta, l1eta, tauVeto, dileptonTrigger, oppositeSign, l0pt;
-  bool allTrue() {
-    return  (grl && larErr && tileErr && ttcVeto && goodVtx && tileTrip && lAr &&
-             badJet && deadRegions && badMuon && cosmicMuon && hfor && ge2blep && eq2blep && mllMin &&
-             l0eta && l1eta && tauVeto && dileptonTrigger && oppositeSign && l0pt);
-  }
-  bool passAllEventCriteria() { return allTrue(); }
-  bool failAny() { return !allTrue(); }
-  std::string str() const;
+    bool allTrue() {
+        return  (passAllEventCriteria() &&
+                 ge2blep && eq2blep && mllMin &&
+                 l0eta && l1eta && tauVeto && dileptonTrigger && oppositeSign && l0pt);
+    }
+    bool passAllEventCriteria() {
+        return (grl && larErr && tileErr && ttcVeto && goodVtx && tileTrip && lAr &&
+                badJet && deadRegions && badMuon && cosmicMuon && hfor);
+    }
+    bool failAny() { return !allTrue(); }
+    std::string str() const;
 };
 
 } // namespace hlfv
