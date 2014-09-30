96dc434ce95486cbcce160692838dfc6d54742ad
Davide Gerbaudo <davide.gerbaudo@gmail.com>
2014-09-30 16:47:31 -0700

    add template used by plot_emu.py --batch

---
+++ run/batch/templates/plot_emu.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#SBATCH -p atlas_all
+#SBATCH --mem-per-cpu=1gb
+#SBATCH --time=04:00:00
+#SBATCH -o %(logfile)s
+#SBATCH --job-name=%(jobname)s
+
+echo "Starting on `hostname`, `date`"
+cd ${SLURM_SUBMIT_DIR}
+./python/plot_emu.py %(opt)s
+echo "Done, `date`"
