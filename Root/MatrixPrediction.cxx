gerbaudo/susynt-ss2lb

commit a3ee067d55b4faba98408144c48d2dfe683bf7b9

move m_computeSystematics from MatrixPrediction to Selector
Now that I also want to run the syst for the other (non-fake) samples,
it makes more sense to have this option in the base class.

Root/MatrixPrediction.cxx
@@ -30,8 +30,7 @@ MatrixPrediction::MatrixPrediction() :
    m_dbg(false),
    m_matrix(0),
    m_use2dparametrization(false),
-   m_allconfigured(false),
-   m_computeSystematics(false)
+   m_allconfigured(false)
 {
 }
 //----------------------------------------------------------

Root/Selector.cxx
@@ -39,6 +39,7 @@ Selector::Selector() :
   m_trigObj(NULL),
   m_mcWeighter(NULL),
   m_useExistingList(false),
+  m_computeSystematics(false),
   m_tupleMaker("",""),
   m_writeTuple(false),
   m_outTupleFile(""),

SusyntHlfv/MatrixPrediction.h
@@ -72,7 +72,6 @@ static std::string FAKESYSNames[nFakeSys] =
                       susy::fake::Systematic::Value);
     MatrixPrediction& setMatrixFilename(const std::string filename); ///< to be called before Begin
     MatrixPrediction& use2dParametrization() { m_use2dparametrization = true; return *this; }
-    MatrixPrediction& useComputeSystematics(bool v) { m_computeSystematics = v; return *this; }
     static std::string dilepDetails(const Susy::Event &event, const DiLepEvtType &ll,
                                     const LeptonVector &ls);
     std::string eventDetails(bool passSrSs, const Susy::Event &event, const DiLepEvtType &ll,
@@ -86,7 +85,6 @@ static std::string FAKESYSNames[nFakeSys] =
     std::string m_matrixFilename;
     bool m_use2dparametrization;
     bool m_allconfigured;
-    bool m_computeSystematics;
     bool initMatrixTool();
     /// compute all the weight variations for the fake systematics
     hlfv::WeightVariations computeSystematicWeights(const susy::fake::Lepton &l0, const susy::fake::Lepton &l1, size_t regionIndex);

SusyntHlfv/Selector.h
@@ -46,6 +46,7 @@ public:
     virtual void    Terminate();             ///< called after looping is finished
     virtual Bool_t  Process(Long64_t entry); ///< called at each event
     Selector& setEventListFilename(const std::string filename);
+    Selector& useComputeSystematics(bool v) { m_computeSystematics = v; return *this; }
     virtual void setDebug(int dbg); ///< overload SusyNtAna::setDebug
     /// toggle ouput ntuple option
     Selector& setWriteNtuple(bool val) { m_writeTuple = val; return *this; }
@@ -193,6 +194,7 @@ protected:
     std::string m_eventListFilename; ///< name of the file with the eventlist (empty string means don't use this feature)
     bool m_useExistingList;        ///< to keep track of whether there is already an event list
     Susy::EventlistHandler m_eventList; ///< the actual event list
+    bool m_computeSystematics; ///< whether the syst (weights for now) should be filled
     hlfv::TupleMaker m_tupleMaker; ///< writer of our analysis nutples
     bool m_writeTuple; ///< whether we want to write the output ntuple
     std::string m_outTupleFile; ///< name of the file where the nutple will be written

util/run_Selector.cxx
@@ -31,6 +31,7 @@ void print_usage(const char *exeName) {
         <<"\t -i [--input]   <file.txt>     : input root file (or filelist or dir) \n"
         <<"\t -n [--num-events] <int>       : number of events to be processed \n"
         <<"\t -s [--sample]  <samplename>   : sample name \n"
+        <<"\t -S [--systematics]            : store also syst variations"<<endl
         <<"\t -t [--tuple-out] fname.root (out ntuple file)\n"
         <<"\t -e [--event-list] <file.root> : file where the eventlist is cached \n"
         <<"Example: \n"
@@ -48,6 +49,7 @@ int main(int argc, char** argv) {
     bool savefake=false;
     bool verbose=false;
     bool debug=false;
+    bool systematics=false;
     string sample;
     string input;
     string output;
@@ -61,6 +63,7 @@ int main(int argc, char** argv) {
         {"save-fake"   ,no_argument,      0, 'f'},
         {"verbose",    no_argument,       0, 'v'},
         {"debug",      no_argument,       0, 'd'},
+        {"systematics",no_argument,       0, 'S'},
         {"input",      required_argument, 0, 'i'},
         {"num-events", required_argument, 0, 'n'},
         {"sample",     required_argument, 0, 's'},
@@ -74,6 +77,7 @@ int main(int argc, char** argv) {
         case 'f' : savefake=true; break;
         case 'v' : verbose=true; break;
         case 'd' : debug=true; break;
+        case 'S' : systematics=true; break;
         case 'i' : input = optarg; break;
         case 'n' : num_events = atoi(optarg); break;
         case 's' : sample = optarg; break;
@@ -90,14 +94,15 @@ int main(int argc, char** argv) {
         cout<<"Being called as : "<<endl;
         for(int i=0; i<argc; ++i) cout<<" "<<argv[i];
         cout<<endl<<"Parsed:"<<endl;
-        cout<<"verbose    '"<<verbose    <<"'"<<endl
-            <<"savefake   '"<<savefake   <<"'"<<endl
-            <<"debug      '"<<debug      <<"'"<<endl
-            <<"input      '"<<input      <<"'"<<endl
-            <<"num-events '"<<num_events <<"'"<<endl
-            <<"sample     '"<<sample     <<"'"<<endl
-            <<"tuple-out  '"<<tuple_out  <<"'"<<endl
-            <<"eventlist  '"<<eventlist  <<"'"<<endl;
+        cout<<"verbose     '"<<verbose    <<"'"<<endl
+            <<"savefake    '"<<savefake   <<"'"<<endl
+            <<"debug       '"<<debug      <<"'"<<endl
+            <<"systematics '"<<systematics<<"'"<<endl
+            <<"input       '"<<input      <<"'"<<endl
+            <<"num-events  '"<<num_events <<"'"<<endl
+            <<"sample      '"<<sample     <<"'"<<endl
+            <<"tuple-out   '"<<tuple_out  <<"'"<<endl
+            <<"eventlist   '"<<eventlist  <<"'"<<endl;
     }
     TChain* chain = new TChain("susyNt");
     ChainHelper::addInput(chain, input, verbose);
@@ -106,7 +111,8 @@ int main(int argc, char** argv) {
     if(debug) chain->ls();
 
     hlfv::Selector selector;
-    if(debug) selector.setDebug(1);
+    selector.setDebug(debug);
+    selector.useComputeSystematics(systematics);
     if(savefake) selector.selectBaselineNonPromptLeptons();
     selector.setSampleName(sample);
     cout<<"eventlist : "<<eventlist<<endl;

commit 75d642e542bd222ef15aba9d8c4a3cdddddc61c1

MatrixPrediction: also store the fake frac kin components
Also two minor bugfixes:
- fix a bug introduced in a3ee067, where the object variation ntuples
  were bein initialized also for MatrixPrediction
- the 'writetuple' and 'tuple-out' options are not necessary for the
  matrix prediction because MatrixPrediction always does write an
  ntuple (and we use 'output' as the tuple filename)

Root/MatrixPrediction.cxx
@@ -32,13 +32,19 @@ MatrixPrediction::MatrixPrediction() :
    m_use2dparametrization(false),
    m_allconfigured(false)
 {
+    m_writeTuple = true; // always the case
 }
 //----------------------------------------------------------
 void MatrixPrediction::Begin(TTree* /*tree*/)
 {
   if(m_dbg) cout << "MatrixPrediction::Begin" << endl;
-  Selector::Begin(0);
-  m_allconfigured = initMatrixTool();
+  // perform all the  Selector::Begin() steps, except initSystematicsList, since we only have weight systematics
+  SusyNtAna::Begin(0);
+  m_allconfigured = true;
+  m_allconfigured &= initChargeFlipTool();
+  m_allconfigured &= initDilTrigLogic();
+  m_allconfigured &= initTupleWriters();
+  m_allconfigured &= initMatrixTool();
 }
 //----------------------------------------------------------
 bool leptonIsFromPv(const Lepton &l)
@@ -255,6 +261,16 @@ hlfv::WeightVariations MatrixPrediction::computeSystematicWeights(const sf::Lept
         wv.fakeElFracDo = m_matrix->getTotalFake(l0, l1, ri, Systematic::SYS_EL_FRAC_DO ) * in;
         wv.fakeMuFracUp = m_matrix->getTotalFake(l0, l1, ri, Systematic::SYS_MU_FRAC_UP ) * in;
         wv.fakeMuFracDo = m_matrix->getTotalFake(l0, l1, ri, Systematic::SYS_MU_FRAC_DO ) * in;
+        wv.fakeMuFrKin1 = m_matrix->getTotalFake(l0, l1, ri, Systematic::SYS_EL_FR_KIN_1) * in ;
+        wv.fakeMuFrKin2 = m_matrix->getTotalFake(l0, l1, ri, Systematic::SYS_MU_FR_KIN_1) * in ;
+        wv.fakeMuFrKin3 = m_matrix->getTotalFake(l0, l1, ri, Systematic::SYS_EL_FR_KIN_2) * in ;
+        wv.fakeMuFrKin4 = m_matrix->getTotalFake(l0, l1, ri, Systematic::SYS_MU_FR_KIN_2) * in ;
+        wv.fakeMuFrKin5 = m_matrix->getTotalFake(l0, l1, ri, Systematic::SYS_EL_FR_KIN_3) * in ;
+        wv.fakeElFrKin1 = m_matrix->getTotalFake(l0, l1, ri, Systematic::SYS_MU_FR_KIN_3) * in ;
+        wv.fakeElFrKin2 = m_matrix->getTotalFake(l0, l1, ri, Systematic::SYS_EL_FR_KIN_4) * in ;
+        wv.fakeElFrKin3 = m_matrix->getTotalFake(l0, l1, ri, Systematic::SYS_MU_FR_KIN_4) * in ;
+        wv.fakeElFrKin4 = m_matrix->getTotalFake(l0, l1, ri, Systematic::SYS_EL_FR_KIN_5) * in ;
+        wv.fakeElFrKin5 = m_matrix->getTotalFake(l0, l1, ri, Systematic::SYS_MU_FR_KIN_5) * in ;
     }
     return wv;
 }

util/run_MatrixPrediction.cxx
@@ -25,8 +25,7 @@ void usage(const char *exeName, const char *defaultMatrixFile) {
       <<"\t"<<"-n [--num-event]   nEvt (default -1, all)"<<endl
       <<"\t"<<"-k [--num-skip]    nSkip (default 0)"     <<endl
       <<"\t"<<"-i [--input]       (file, list, or dir)"  <<endl
-      <<"\t"<<"-o [--output]      output file"           <<endl
-      <<"\t"<<"-t [--tuple-out] fname.root (out ntuple file)"<<endl
+      <<"\t"<<"-o [--output]      output file (required)"<<endl
       <<"\t"<<"-s [--sample]      samplename"            <<endl
       <<"\t"<<"-S [--systematics] : store also syst variations"<<endl
       <<"\t"<<"-e [--etapt]       : use eta-pt (default pt only)"<<endl
@@ -44,7 +43,6 @@ int main(int argc, char** argv)
   string sample;
   string input;
   string output;
-  bool writeTuple = false;
   bool etapt = false;
   bool systematics = false;
   string matrixFile(hlfv::getRootCoreDir()+"/data/DileptonMatrixMethod/FakeMatrix_Jul_26.root");
@@ -61,7 +59,6 @@ int main(int argc, char** argv)
     else if(sw=="-o"||sw=="--output"     ) { output = argv[++optind]; }
     else if(sw=="-s"||sw=="--sample"     ) { sample = argv[++optind]; }
     else if(sw=="-S"||sw=="--systematics") { systematics = true; }
-    else if(sw=="-t"||sw=="--tuple-out")   { writeTuple = true; output = argv[++optind]; }
     else if(sw=="-h"||sw=="--help"       ) { usage(argv[0], matrixFile.c_str()); return 0; }
     else cout<<"Unknown switch "<<sw<<endl;
     optind++;
@@ -75,9 +72,12 @@ int main(int argc, char** argv)
       <<"  input   "<<input     <<endl
       <<"  output  "<<output    <<endl
       <<"  systematics "<<systematics<<endl
-      <<"  writeTuple  "<<writeTuple <<endl
       <<endl;
 
+  if(!output.length()){
+      cout<<"--output is a required option"<<endl;
+      return 1;
+  }
   TChain* chain = new TChain("susyNt");
   bool verbose(dbg>0);
   ChainHelper::addInput(chain, input, verbose);