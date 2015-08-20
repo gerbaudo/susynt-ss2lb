23cf0b03b415672fec2a001cd3ce76dbd776ff48
gerbaudo <davide.gerbaudo@gmail.com>
2015-08-20 19:24:36 +0200

    minor patches to previous 3 commits

--- Root/FakeTuplizer.cxx
+++ Root/FakeTuplizer.cxx
@@ -3,7 +3,6 @@
 #include "susynt-ss2lb/EventFlags.h"
 #include "susynt-ss2lb/DileptonVariables.h"
 
-
 using namespace std;
 using hlfv::FakeTuplizer;
 using hlfv::Selector;
@@ -16,7 +15,7 @@ using hlfv::DileptonVariables;
 FakeTuplizer::FakeTuplizer() :
     m_tupleMakerEmu("", "")
 {
-  setAnaType(Ana_2LepWH);
+  nttools().setAnaType(Susy::AnalysisType::Ana_2LepWH);
   setSelectTaus(true);
 }
 //-----------------------------------------
@@ -42,7 +41,7 @@ Bool_t FakeTuplizer::Process(Long64_t entry)
     assignStaticWeightComponents(nt, *m_mcWeighter, weightComponents);
     m_counter.increment(weightComponents.product(), "input");
     bool removeLepsFromIso(false);
-    selectObjects(NtSys_NOM, removeLepsFromIso, TauID_medium); // always select with nominal? (to compute event flags)
+    selectObjects(Susy::NtSys::NOM, removeLepsFromIso, TauID_medium); // always select with nominal? (to compute event flags)
     EventFlags eventFlags = computeEventFlags();
     incrementEventCounters(eventFlags, weightComponents);
     if(eventFlags.passAllEventCriteria()) {
@@ -50,14 +49,14 @@ Bool_t FakeTuplizer::Process(Long64_t entry)
         const JetVector&   bj = m_baseJets; // why are we using basejets and not m_signalJets2Lep?
         const LeptonVector& l = m_signalLeptons;
         if(l.size()==2 && eventIsEmu(l)) { // several vars cannot be computed if we don't have 2 lep
-            const JetVector cljets(Selector::filterJets(m_signalJets2Lep, m_jvfTool, sys, m_anaType));
+            const JetVector cljets(Selector::filterJets(m_signalJets2Lep, m_jvfTool, sys, nttools().getAnaType()));
             DileptonVariables vars = computeDileptonVariables(l, m_met, cljets, m_signalJets2Lep, m_signalTaus);
             assignNonStaticWeightComponents(l, bj, sys, vars, weightComponents);
             incrementObjectSplitCounters(vars, weightComponents);
             if(usingEventList() && !m_useExistingList) m_eventList.addEvent(entry);
             if(m_writeTuple) {
                 double weight(weightComponents.product());
-                unsigned int run(nt.evt()->run), event(nt.evt()->event);
+                unsigned int run(nt.evt()->run), event(nt.evt()->eventNumber);
                 const Lepton &l0 = *m_signalLeptons[0];
                 const Lepton &l1 = *m_signalLeptons[1];
                 m_tupleMaker
