c3bc06b1a8981ab25287337545d5aae76d22765f
gerbaudo <davide.gerbaudo@gmail.com>
2015-09-23 18:39:47 +0200

    rewrite using classes
    Will make is easier to filter collections and produce full cutflow

--- run/python/cutflow_ximo_ntuple.py
+++ run/python/cutflow_ximo_ntuple.py
@@ -26,7 +26,6 @@ from rootUtils import (drawAtlasLabel
                        ,getXrange
                        ,referenceLine
                        )
-r = importRoot()
 from utils import (commonPrefix
                    ,commonSuffix
                    ,dictSum
@@ -62,11 +61,38 @@ simple cutflow, run with -h
     chain = R.TChain(treename)
     R.ChainHelper.addInput(chain, opts.input, opts.verbose)
     print "Number of events in the input chain: {0}".format(chain.GetEntries())
+
+    eventlist = None
+    eventIndices = xrange(chain.GetEntries())
+    # if True:
+    #     eventlist = R.TEventList("ss3lEventlist", "events selected")
+    #     chain.Draw(">>"+eventlist.GetName(),
+    #                "RunNb==270806 && "
+    #                "(EventNumber==5600684 ||"
+    #                " EventNumber==7314718 ||"
+    #                " EventNumber==8903766 )",
+    #                'groff')
+    #     eventIndices = [eventlist.GetList()[i] for i in xrange(eventlist.GetN())]
+    # for iEvent, iEntry in enumerate(eventIndices):
     for iEvent, event in enumerate(chain):
-        print "run {0} event {1}".format(event.RunNb, event.EventNumber)
+        # chain.GetEntry(iEntry)
+        # if chain.RunNb!=270806 or chain.EventNumber not in [5600684, 7314718, 8903766]:
+        if (chain.RunNb!=270806 or
+            chain.EventNumber not in [7684284, 16022745, 4073193, 14768930, 16595938]):
+            continue
+        print "run {0} event {1}".format(chain.RunNb, chain.EventNumber)
+        electrons = Electron.build_electrons(chain)
+        muons = Muon.build_muons(chain)
+        jets = Jet.build_jets(chain)
+
+        print_muons(muons)
+        print_electrons(electrons)
+        print_jets(jets)
+
         # if iEvent>10:
         #     break
-    
+
+
 def printCounters(counters):
     countTotalBkg(counters)
     blindGroups   = [g for g in counters.keys() if g!='data']
@@ -82,10 +108,7 @@ def printCounters(counters):
     print 4*'-',' blind regions ',4*'-'
     print tableBld.csv()
 
-if __name__=='__main__' :
-    main()
-
-# # for reference: branch names v18
+# # for reference: branch names v18 (the ones that are not in Electron, Muon, Jet)
 # L1_2EM15
 # L1_2EM7
 # L1_2MU4
@@ -174,82 +197,6 @@ if __name__=='__main__' :
 # passGRL
 # RunNb
 # DetError
-# NMu
-# Mu_eta
-# Mu_phi
-# Mu_pT
-# Mu_SFw
-# Mu_charge
-# Mu_d0pvtx
-# Mu_sigd0
-# Mu_sigd0old
-# Mu_z0pvtx
-# Mu_isBad
-# Mu_isCosmic
-# Mu_ptcone20
-# Mu_ptcone30
-# Mu_ptcone40
-# Mu_ptvarcone20
-# Mu_ptvarcone30
-# Mu_ptvarcone40
-# Mu_topoetcone20
-# Mu_topoetcone30
-# Mu_topoetcone40
-# Mu_passIsoLooseTO
-# Mu_passIsoLoose
-# Mu_passIsoTight
-# Mu_passIsoGrad
-# Mu_passIsoGradLoose
-# MuTrigSF_2mu14
-# MuTrigSF_mu14
-# NEl
-# El_eta
-# El_etaclus
-# El_phi
-# El_pT
-# El_E
-# El_charge
-# El_sigd0
-# El_sigd0old
-# El_z0pvtx
-# El_d0pvtx
-# El_SFwMedium
-# El_SFwMediumLH
-# El_SFwTight
-# El_SFwTightLH
-# El_isLooseLH
-# El_isMediumLH
-# El_isTightLH
-# El_isLoose
-# El_isMedium
-# El_isTight
-# El_nBLayerHits
-# El_expectBLayerHit
-# El_ptcone20
-# El_ptcone30
-# El_ptcone40
-# El_ptvarcone20
-# El_ptvarcone30
-# El_ptvarcone40
-# El_topoetcone20
-# El_topoetcone30
-# El_topoetcone40
-# El_passIsoLooseTO
-# El_passIsoLoose
-# El_passIsoTight
-# El_passIsoGrad
-# El_passIsoGradLoose
-# NJet
-# Jet_eta
-# Jet_phi
-# Jet_pT
-# Jet_E
-# Jet_quality
-# Jet_JVF
-# Jet_JVT
-# Jet_MV2c20
-# Jet_SFw
-# Jet_nTrk
 # Etmiss_CST_Etx
 # Etmiss_CST_Ety
 # Etmiss_CST_Et
@@ -257,3 +204,231 @@ if __name__=='__main__' :
 # Etmiss_TST_Ety
 # Etmiss_TST_Et
 # PV_z
+
+
+
+#-----------------------------------------------------------
+class Electron(object):
+    "wraps electron from flat ntuple"
+
+    def __init__(self, event, index):
+        for attribute in Electron.attribute_names():
+            prefix = Electron.prefix()
+            setattr(self, attribute, getattr(event, prefix+attribute)[index])
+        MeV2GeV = 1.0e-3
+        self.pT *= MeV2GeV
+        self.E *= MeV2GeV
+
+    @staticmethod
+    def size_leaf():
+        return 'NEl'
+
+    @staticmethod
+    def prefix():
+        return 'El_'
+
+    @staticmethod
+    def attribute_names():
+        return [
+            'pT'
+            ,'eta'
+            ,'etaclus'
+            ,'phi'
+            ,'E'
+            ,'charge'
+            ,'sigd0'
+            # ,'sigd0old'
+            ,'z0pvtx'
+            # ,'d0pvtx'
+            # ,'SFwMedium'
+            # ,'SFwMediumLH'
+            # ,'SFwTight'
+            # ,'SFwTightLH'
+            ,'isLooseLH'
+            ,'isMediumLH'
+            ,'isTightLH'
+            # ,'isLoose'
+            # ,'isMedium'
+            # ,'isTight'
+            # ,'nBLayerHits'
+            # ,'expectBLayerHit'
+            # ,'ptcone20'
+            # ,'ptcone30'
+            # ,'ptcone40'
+            # ,'ptvarcone20'
+            # ,'ptvarcone30'
+            # ,'ptvarcone40'
+            # ,'topoetcone20'
+            # ,'topoetcone30'
+            # ,'topoetcone40'
+            # ,'passIsoLooseTO'
+            # ,'passIsoLoose'
+            # ,'passIsoTight'
+            # ,'passIsoGrad'
+            # ,'passIsoGradLoose'
+        ]
+    @staticmethod
+    def build_electrons(event):
+        n_electrons = getattr(event, Electron.size_leaf())
+        print 'n_electrons ',n_electrons
+        return [Electron(event, i) for i in xrange(n_electrons)]
+    @staticmethod
+    def column_width():
+        return 5
+
+    @staticmethod
+    def header():
+        cell = '%'+str(Electron.column_width())+'s'
+        return ' '.join([cell % a for a in Electron.attribute_names()])
+
+    def str(self):
+        cell = '%'+str(Electron.column_width())+'.2f'
+        return ' '.join([cell % getattr(self, a) for a in Electron.attribute_names()])
+
+def print_electrons(electrons):
+    print 'electrons: '+('--' if not electrons else '')
+    if electrons:
+        w = Electron.column_width()
+        print w*' '+Electron.header()
+        template_line = '[%'+str(w)+'s] %s'
+        print '\n'.join([template_line % (i, m.str()) for i, m in enumerate(electrons)])
+
+#-----------------------------------------------------------
+class Muon(object):
+    "wraps muons from flat ntuple"
+
+    def __init__(self, event, index):
+        for attribute in Muon.attribute_names():
+            prefix = Muon.prefix()
+            setattr(self, attribute, getattr(event, prefix+attribute)[index])
+        MeV2GeV = 1.0e-3
+        self.pT *= MeV2GeV
+
+    @staticmethod
+    def size_leaf():
+        return 'NMu'
+
+    @staticmethod
+    def prefix():
+        return 'Mu_'
+
+    @staticmethod
+    def attribute_names():
+        return [
+            'pT'
+            ,'eta'
+            ,'phi'
+            # ,'SFw'
+            # ,'charge'
+            # ,'d0pvtx'
+            ,'sigd0'
+            # ,'sigd0old'
+            ,'z0pvtx'
+            # ,'isBad'
+            # ,'isCosmic'
+            # ,'ptcone20'
+            # ,'ptcone30'
+            # ,'ptcone40'
+            # ,'ptvarcone20'
+            # ,'ptvarcone30'
+            # ,'ptvarcone40'
+            # ,'topoetcone20'
+            # ,'topoetcone30'
+            # ,'topoetcone40'
+            # ,'passIsoLooseTO'
+            # ,'passIsoLoose'
+            # ,'passIsoTight'
+            # ,'passIsoGrad'
+            # ,'passIsoGradLoose'
+            ]
+    @staticmethod
+    def build_muons(event):
+        n_muons = getattr(event, Muon.size_leaf())
+        print 'n_muons ',n_muons
+        return [Muon(event, i) for i in xrange(n_muons)]
+
+    @staticmethod
+    def column_width():
+        return 5
+
+    @staticmethod
+    def header():
+        cell = '%'+str(Muon.column_width())+'s'
+        return ' '.join([cell % a for a in Muon.attribute_names()])
+
+    def str(self):
+        cell = '%'+str(Muon.column_width())+'.2f'
+        return ' '.join([cell % getattr(self, a) for a in Muon.attribute_names()])
+
+def print_muons(muons):
+    print 'muons: '+('--' if not muons else '')
+    if muons:
+        w = Muon.column_width()
+        print w*' '+Muon.header()
+        template_line = '[%'+str(w)+'s] %s'
+        print '\n'.join([template_line % (i, m.str()) for i, m in enumerate(muons)])
+#-----------------------------------------------------------
+class Jet(object):
+    "wraps jets from flat ntuple"
+
+    def __init__(self, event, index):
+        for attribute in Jet.attribute_names():
+            prefix = Jet.prefix()
+            setattr(self, attribute, getattr(event, prefix+attribute)[index])
+        MeV2GeV = 1.0e-3
+        self.pT *= MeV2GeV
+        self.E *= MeV2GeV
+
+    @staticmethod
+    def size_leaf():
+        return 'NJet'
+
+    @staticmethod
+    def prefix():
+        return 'Jet_'
+
+    @staticmethod
+    def attribute_names():
+        return [
+            'eta'
+            ,'phi'
+            ,'pT'
+            ,'E'
+            # ,'quality'
+            # ,'JVF'
+            # ,'JVT'
+            # ,'MV2c20'
+            # ,'SFw'
+            ,'nTrk'
+            ]
+
+    @staticmethod
+    def build_jets(event):
+        n_jets = getattr(event, Jet.size_leaf())
+        print 'n_jets ',n_jets
+        return [Jet(event, i) for i in xrange(n_jets)]
+
+    @staticmethod
+    def column_width():
+        return 5
+
+    @staticmethod
+    def header():
+        cell = '%'+str(Jet.column_width())+'s'
+        return ' '.join([cell % a for a in Jet.attribute_names()])
+
+    def str(self):
+        cell = '%'+str(Jet.column_width())+'.2f'
+        return ' '.join([cell % getattr(self, a) for a in Jet.attribute_names()])
+
+def print_jets(jets):
+    print 'jets: '+('--' if not jets else '')
+    if jets:
+        w = Jet.column_width()
+        print w*' '+Jet.header()
+        template_line = '[%'+str(w)+'s] %s'
+        print '\n'.join([template_line % (i, m.str()) for i, m in enumerate(jets)])
+
+
+if __name__=='__main__' :
+    main()
